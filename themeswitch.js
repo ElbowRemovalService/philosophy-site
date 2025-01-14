3fd16be7d6a386668facf8e7bd14c839d0615178
Elbow Removal Service <elbowremovalservice@gmail.com>
2025-01-14 10:57:07 -0700

    Theme support!

---
+++ themeswitch.js
@@ -0,0 +1,9 @@
+function f1() {
+    document.getElementById('theme').innerHTML = '<link rel="stylesheet" type="text/css" href="../styles/main.css">\n    <link rel="stylesheet" type="text/css" href="../styles/purple.css">'
+}
+function f2() {
+    document.getElementById('theme').innerHTML = '<link rel="stylesheet" type="text/css" href="../styles/main.css">\n    <link rel="stylesheet" type="text/css" href="../styles/gold.css">'
+}
+function f3() {
+    document.getElementById('theme').innerHTML = '<link rel="stylesheet" type="text/css" href="../styles/main.css">\n    <link rel="stylesheet" type="text/css" href="../styles/black.css">'
+}
